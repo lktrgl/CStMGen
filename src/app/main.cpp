3fae303aafe1b7df3421458ddf4aa77351438b85
lktrgl <32882989+lktrgl@users.noreply.github.com>
2020-09-12 23:07:02 +0300

    add lggm submodule

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,9 +1,9 @@
 #include <string>
 #include <vector>
 
-int main(int argc, char** argv)
+int main ( int argc, char** argv )
 {
-    std::vector<std::string> const args(argv, argv+argc);
-    
-    return 0;
+  std::vector<std::string> const args ( argv, argv + argc );
+
+  return 0;
 }
