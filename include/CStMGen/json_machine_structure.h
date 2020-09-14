8f73765879ca3b8bb4d9763823d7d27dbad5c245
lktrgl <32882989+lktrgl@users.noreply.github.com>
2020-09-15 01:00:21 +0300

    added json_machine_structure_t skeleton implementation

---
+++ include/CStMGen/json_machine_structure.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <string>
+#include <string_view>
+#include <map>
+#include <vector>
+
+namespace cfg
+{
+
+class json_machine_structure_t final
+{
+  using state_id_t = std::string;
+  using state_value_t = std::string;
+  using states_t = std::map<state_id_t, state_value_t>;
+  using transitions_t = std::multimap<state_id_t, state_id_t>;
+
+  constexpr static std::string_view const m_key_machine_name = "machine-name";
+  constexpr static std::string_view const m_key_states = "states";
+  constexpr static std::string_view const m_key_transitions = "transitions";
+
+  constexpr static std::string_view const m_key_id = "id";
+  constexpr static std::string_view const m_key_value = "value";
+
+  constexpr static std::string_view const m_key_from = "from";
+  constexpr static std::string_view const m_key_to = "to";
+
+public:
+  json_machine_structure_t ( std::string const& config_file_pathname );
+
+  json_machine_structure_t ( json_machine_structure_t const& ) = delete;
+  json_machine_structure_t& operator= ( json_machine_structure_t const& ) = delete;
+  json_machine_structure_t ( json_machine_structure_t&& ) = delete;
+  json_machine_structure_t& operator= ( json_machine_structure_t&& ) = delete;
+
+private:
+  void import ( std::string const& config_file_pathname );
+
+private:
+  std::string const m_config_file_pathname;
+
+  std::string m_machine_name;
+  states_t m_states;
+  transitions_t m_transitions_from;
+};
+
+} // namespace cfg
---
+++ src/CStMGen/json_machine_structure.cpp
@@ -0,0 +1,125 @@
+#include <CStMGen/json_machine_structure.h>
+
+#include <rapidjson/document.h>
+
+#include <iostream>
+#include <fstream>
+
+namespace cfg
+{
+
+json_machine_structure_t::json_machine_structure_t ( std::string const& config_file_pathname )
+  : m_config_file_pathname ( config_file_pathname )
+{
+  import ( m_config_file_pathname );
+}
+
+void json_machine_structure_t::import ( std::string const& config_file_pathname )
+{
+  std::ifstream in { config_file_pathname, std::ios::binary | std::ios::ate};
+
+  if ( !in.is_open() )
+  {
+    std::cerr << "cannot open the '" << config_file_pathname << "' file." << std::endl;
+    return;
+  }
+
+  size_t const in_length = in.tellg();
+  in.seekg ( 0, std::ios::beg );
+
+  std::vector<char> buff ( in_length + 1 );
+
+  in.read ( buff.data(), buff.size() );
+
+  rapidjson::Document d;
+  d.Parse ( buff.data() );
+
+  {
+    // retrieve the "machine-name"
+    rapidjson::Value& s = d[m_key_machine_name.data()];
+
+    if ( not s.IsString() )
+    {
+      std::cerr << "the '" << m_key_machine_name << "' property is not a string." << std::endl;
+      return;
+    }
+
+    m_machine_name = s.GetString();
+
+    if ( not m_machine_name.length() )
+    {
+      std::cerr << "the '" << m_key_machine_name << "' property is empty." << std::endl;
+      return;
+    }
+  }
+
+  {
+    // retrieve the "states"
+    rapidjson::Value& a = d[m_key_states.data()];
+
+    if ( not a.IsArray() )
+    {
+      std::cerr << "the '" << m_key_states << "' property is not an array." << std::endl;
+      return;
+    }
+
+    for ( rapidjson::Value::ValueIterator itr = a.Begin(); itr != a.End(); ++itr )
+    {
+      rapidjson::Value& obj = *itr;
+
+      if ( not obj.IsObject() )
+      {
+        std::cerr << "item of the '" << m_key_states << "' array is not a object." << std::endl;
+        continue;
+      }
+
+      rapidjson::Value::ConstMemberIterator id = obj.FindMember ( m_key_id.data() );
+      rapidjson::Value::ConstMemberIterator value = obj.FindMember ( m_key_value.data() );
+
+      if ( obj.MemberEnd() == id or not id->value.IsString()
+           or obj.MemberEnd() == value or not value->value.IsString() )
+      {
+        std::cerr << "item of the '" << m_key_states << "' does not have expected members." << std::endl;
+        continue;
+      }
+
+      m_states.insert ( std::make_pair ( id->value.GetString(), value->value.GetString() ) );
+    } // for itr
+  }
+
+  {
+    // retrieve the "transitions"
+    rapidjson::Value& a = d[m_key_transitions.data()];
+
+    if ( not a.IsArray() )
+    {
+      std::cerr << "the '" << m_key_transitions << "' property is not an array." << std::endl;
+      return;
+    }
+
+    for ( rapidjson::Value::ValueIterator itr = a.Begin(); itr != a.End(); ++itr )
+    {
+      rapidjson::Value& obj = *itr;
+
+      if ( not obj.IsObject() )
+      {
+        std::cerr << "item of the '" << m_key_transitions << "' array is not a object." << std::endl;
+        continue;
+      }
+
+      rapidjson::Value::ConstMemberIterator from = obj.FindMember ( m_key_from.data() );
+      rapidjson::Value::ConstMemberIterator to = obj.FindMember ( m_key_to.data() );
+
+      if ( obj.MemberEnd() == from or not from->value.IsString()
+           or obj.MemberEnd() == to or not to->value.IsString() )
+      {
+        std::cerr << "item of the '" << m_key_transitions << "' does not have expected members." << std::endl;
+        continue;
+      }
+
+      m_transitions_from.insert ( std::make_pair ( from->value.GetString(), to->value.GetString() ) );
+    } // for itr
+  }
+}
+
+} // namespace cfg
