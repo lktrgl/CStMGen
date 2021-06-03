6828a1eea2fd772e17c89027582d49420ef0a76b
lktrgl <32882989+lktrgl@users.noreply.github.com>
2021-06-03 20:59:41 +0300

    all the format templates are grouped as a single header file

--- src/CStMGen/cstmgen_process.cpp
+++ src/CStMGen/cstmgen_process.cpp
@@ -1,37 +1,10 @@
 #include <CStMGen/cstmgen_process.h>
 
-#include <CStMGen/data/templates/state_machine_enum/cstm_state_enum_template_h.h>
-#include <CStMGen/data/templates/state_machine_enum/cstm_state_enum_state_name_text.h>
-#include <CStMGen/data/templates/state_machine_enum/cstm_state_enum_file_name_template_h.h>
+/* ------------------------------------------------------------------------- */
 
-#include <CStMGen/data/templates/state_machine_data/cstm_state_data_desc_file_name_template_h.h>
-#include <CStMGen/data/templates/state_machine_data/cstm_state_data_desc_file_name_template_c.h>
-#include <CStMGen/data/templates/state_machine_data/cstm_state_data_desc_template_h.h>
-#include <CStMGen/data/templates/state_machine_data/cstm_state_data_desc_template_c.h>
-#include <CStMGen/data/templates/state_machine_data/cstm_state_data_decl_placeholder_name_template.h>
-#include <CStMGen/data/templates/state_machine_data/cstm_state_data_init_placeholder_name_template.h>
+#include <CStMGen/cstmgen_process_templates.h>
 
-#include <CStMGen/data/templates/state_diagram_code/cstm_state_diagram_template_h.h>
-#include <CStMGen/data/templates/state_diagram_code/cstm_state_diagram_template_c.h>
-#include <CStMGen/data/templates/state_diagram_code/cstm_state_diagram_file_name_template_h.h>
-#include <CStMGen/data/templates/state_diagram_code/cstm_state_diagram_file_name_template_c.h>
-#include <CStMGen/data/templates/state_diagram_code/cstm_state_include_text_template.h>
-#include <CStMGen/data/templates/state_diagram_code/cstm_state_node_text_template.h>
-
-#include <CStMGen/data/templates/state_code/cstm_state_template_c.h>
-#include <CStMGen/data/templates/state_code/cstm_state_template_h.h>
-#include <CStMGen/data/templates/state_code/cstm_state_file_name_template_h.h>
-#include <CStMGen/data/templates/state_code/cstm_state_file_name_template_c.h>
-#include <CStMGen/data/templates/state_code/cstm_state_transition_template_c.h>
-#include <CStMGen/data/templates/state_code/cstm_state_transition_name_template.h>
-#include <CStMGen/data/templates/state_code/cstm_state_transition_evaluation_placeholder_name_template.h>
-
-#include <CStMGen/data/templates/state_code/cstm_state_global_data_placeholder_name_template.h>
-#include <CStMGen/data/templates/state_code/cstm_state_handler_enter_placeholder_name_template.h>
-#include <CStMGen/data/templates/state_code/cstm_state_handler_input_placeholder_name_template.h>
-#include <CStMGen/data/templates/state_code/cstm_state_handler_leave_placeholder_name_template.h>
-#include <CStMGen/data/templates/state_code/cstm_state_handler_output_placeholder_name_template.h>
-#include <CStMGen/data/templates/state_code/cstm_state_handler_run_placeholder_name_template.h>
+/* ------------------------------------------------------------------------- */
 
 #include <locale>
 #include <iostream>
